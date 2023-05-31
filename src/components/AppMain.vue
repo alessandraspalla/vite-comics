02cddefbcce907e929ec5b83cac9836fcf1dc3fd
alessandraspalla <alespalla93@icloud.com>
2023-05-31 22:45:24 +0200

    add component AppMain - main section with scss

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -4,8 +4,94 @@ export default {
 }
 </script>
 
-<template></template>
+<template>
+    <main>
+        <section id="content">
+            <div class="container">
+                -- CONTENT GOES HERE --
+            </div>
+        </section>
+
+        <section id="shop">
+            <div class="container">
+                <div class="card">
+                    <div class="image">
+                        <img src="/img/buy-comics-digital-comics.png" alt="">
+                    </div>
+                    <div class="text">
+                        DIGITAL COMICS
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="image">
+                        <img src="/img/buy-comics-merchandise.png" alt="">
+                    </div>
+                    <div class="text">
+                        DC MERCHANDISE
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="image">
+                        <img src="/img/buy-comics-subscriptions.png" alt="">
+                    </div>
+                    <div class="text">
+                        SUBSCRIPTION
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="image">
+                        <img src="/img/buy-comics-shop-locator.png" alt="">
+                    </div>
+                    <div class="text">
+                        COMIC SHOP LOCATOR
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="image">
+                        <img src="/img/buy-dc-power-visa.svg" alt="">
+                    </div>
+                    <div class="text">
+                        DC POWER VISA
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</template>
 
 <style lang="scss" scoped>
 @use '../styles/partials/variables.scss' as *;
+
+#content {
+    height: 100px;
+    background-color: $black;
+    color: $white
+}
+
+#shop {
+    height: 120px;
+    background-color: $primary-color;
+
+    .container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 100%;
+
+        .card {
+            display: flex;
+            align-items: center;
+            height: 100%;
+
+            .image {
+                width: 40px;
+            }
+
+            .text {
+                color: $white;
+                margin-inline: 8px;
+            }
+        }
+    }
+}
 </style>
