0d01e21bc0da975cb2edb36a75734e1bab17fac2
alessandraspalla <alespalla93@icloud.com>
2023-06-01 14:20:23 +0200

    add jumbotron

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script>
 import AppHeader from './components/AppHeader.vue'
+import AppHero from './components/AppHero.vue'
 import AppMain from './components/AppMain.vue'
 import AppFooter from './components/AppFooter.vue'
 
 export default {
   components: {
     AppHeader,
+    AppHero,
     AppMain,
     AppFooter
   }
@@ -14,6 +16,7 @@ export default {
 
 <template>
   <AppHeader />
+  <AppHero />
   <AppMain />
   <AppFooter />
 </template>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'AppHero',
+}
+</script>
+
+<template>
+    <div id="jumbotron"></div>
+</template>
+
+<style lang="scss" scoped>
+#jumbotron {
+    height: 270px;
+    background-image: url('/img/jumbotron.jpg');
+    background-size: 100%;
+}
+</style>
